10a46cc508f28f85511a444f9932345667b78d7e
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-05-22 18:30:14 -0400

    Add server.js file and code

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+// Set up Express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
