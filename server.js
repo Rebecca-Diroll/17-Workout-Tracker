8b6ad7b9ea01e5dbd84a3d343f0e158fd5810fab
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-05-26 17:06:53 -0400

    Update server.js with api and html_routes

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
+app.use(require("./routes/html_routes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
