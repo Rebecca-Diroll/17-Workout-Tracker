f0afff070792f4a5a36ecdc0d248ffe29536c249
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-05-26 17:34:09 -0400

    Rearrange routes in api.js

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .sort({ date: -1 })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
 router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
       .then(dbWorkout => {
@@ -22,16 +33,5 @@ router.put("/api/workouts/:id", (req, res) => {
       res.status(400).json(err);
     });
 });
-
-router.get("/api/workouts", (req, res) => {
-    Workout.find({})
-      .sort({ date: -1 })
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
 
   module.exports = router;
