818c0c50a4c356bd006eb2790327d5baaee34572
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-05-26 17:12:03 -0400

    Add workoutSchema to workout.js

--- models/workout.js
+++ models/workout.js
@@ -1,2 +1,43 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    }, 
+    exercises: [
+        {
+            type:{
+                type: String,
+                trim: true,
+                required: "Enter the exercise type: "
+            }, 
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter the exercise name: "
+            },
+            duration: {
+                type: Number,
+                trim: true,
+                required: "Enter the exercise duration in minutes: "
+            },
+            weight: {
+                type: Number,
+                trim: true,
+                required: "Enter the weight: "
+            },  
+            reps: {
+                type: Number,
+                trim: true,
+                required: "Enter the number of reps: "
+            },
+            sets: {
+                type: Number,
+                trim: true,
+                required: "Enter the number of sets: "
+            },
+        }
+    ]
+});
